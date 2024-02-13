b4910ccafe4ebabcb12fc385e5a6f2e1918977ff
Mark B. Jones aka The Scuba Hacker <131517566+scuba-hacker@users.noreply.github.com>
2024-02-13 17:17:38 +0000

    add waypoint category

--- NavigationWaypoints.h
+++ NavigationWaypoints.h
@@ -4,27 +4,39 @@
 #include <stdint.h>
 #include <array>
 
+
+enum eWaypointCategory
+{
+  BLUE_BUOY,
+  NO_BUOY,
+  PLATFORM,
+  CONTAINER,
+  ORANGE_BUOY,
+  JETTY,
+  UNMARKED
+};
+
 class NavigationWaypoint
 {
   public:
 
+
     const char* _label;
     const char* _m5label;
+    eWaypointCategory _cat;
 
     double _lat;
     double _long;
 
-    NavigationWaypoint()
-    {
-      _label = nullptr;
-      _lat = _long = 0.0;
-    }
-
-    NavigationWaypoint(const char*  label, const char* m5label, double latitude, double longitude) : _label(label), _m5label(m5label), _lat(latitude), _long(longitude)
+    NavigationWaypoint() : _label(nullptr), _m5label(nullptr), _cat(UNMARKED),_lat(0.0),_long(0.0) 
     {
     }
 
-    NavigationWaypoint(const char*  label, double latitude, double longitude) : _label(label), _m5label(label), _lat(latitude), _long(longitude)
+    NavigationWaypoint(const char*  label, const char* m5label, eWaypointCategory cat, double latitude, double longitude) : _label(label), _m5label(m5label), _cat(cat), _lat(latitude), _long(longitude)
+    {
+    }
+
+    NavigationWaypoint(const char*  label, double latitude, double longitude) : _label(label), _m5label(label), _lat(latitude), _long(longitude), _cat(UNMARKED)
     {
     }
 };
